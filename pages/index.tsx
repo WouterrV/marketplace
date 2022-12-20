216b00a72bd571d5a22c4b5d11d8bf0278c06b88
Wouter <wouter@example.com>
2022-12-20 17:34:15 +0100

    cleanup: remove unused styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,7 @@
 
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
 import { css } from '@emotion/react'
-
-const inter = Inter({ subsets: ['latin'] })
 
 const color = 'white'
 
@@ -27,77 +23,16 @@ export default function Home() {
             </Head>
             <main>
                 <div
-                    css={css`
-                        padding: 32px;
-                        background-color: hotpink;
-                        font-size: 24px;
-                        border-radius: 4px;
-                        &:hover {
-                            color: ${color};
-                        }
-                    `}
+                    css={{
+                        padding: '32px',
+                        'background-color': 'turquoise',
+                        '&:hover': {
+                            color: color,
+                        },
+                    }}
                 >
+                    <h1>Marketplace</h1>
                     <p>Best offers EU</p>
-                </div>
-
-                <div>Marketplace</div>
-
-                <div>
-                    <a
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Docs <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Find in-depth information about Next.js features
-                            and&nbsp;API.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Learn <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Learn about Next.js in an interactive course
-                            with&nbsp;quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Templates <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Discover and deploy boilerplate example
-                            Next.js&nbsp;projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Deploy <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Instantly deploy your Next.js site to a shareable
-                            URL with&nbsp;Vercel.
-                        </p>
-                    </a>
                 </div>
             </main>
         </>
