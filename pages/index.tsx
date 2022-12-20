c290a6051590ef93c0926256bd5f5791966624d2
Wouter <wouter@example.com>
2022-12-20 17:56:36 +0100

    WIP form, menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ import { css } from '@emotion/react'
 
 const color = 'white'
 
+function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log('searching, event: ', event)
+}
+
 export default function Home() {
     return (
         <>
@@ -21,19 +26,25 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <div
+                css={{
+                    padding: '32px',
+                    'background-color': 'turquoise',
+                    '&:hover': {
+                        color: color,
+                    },
+                }}
+            >
+                <h1>Marketplace</h1>
+                (login, see favourite listings)
+            </div>
             <main>
-                <div
-                    css={{
-                        padding: '32px',
-                        'background-color': 'turquoise',
-                        '&:hover': {
-                            color: color,
-                        },
-                    }}
-                >
-                    <h1>Marketplace</h1>
-                    <p>Best offers EU</p>
-                </div>
+                <form onSubmit={handleSearch}>
+                    <input type="text" placeholder="Search" />
+                    <button>Search</button>
+                </form>
+
+                <p>Recent/relevant for you offers go here</p>
             </main>
         </>
     )
