143f0f8bfc3936181f9b5cb87cb1cc725fb1c94b
Wouter <wouter@example.com>
2023-01-16 16:08:09 +0100

    feat: upload images to nhost

--- components/ImageForm.tsx
+++ components/ImageForm.tsx
@@ -6,22 +6,49 @@ import Dropzone from './Dropzone'
 // Chakra
 import { Flex, Button } from '@chakra-ui/react'
 
+// Nhost
+import { useUserData, useFileUpload } from '@nhost/react'
+import { NhostContext } from '../pages/_app'
+
 type TImageFormProps = {
     file: File | null
     setFile: (file: File | null) => void
 }
 
 const ImageForm = ({ file, setFile }: TImageFormProps) => {
-    const onFileAccepted = (file: File) => {
-        console.log('file accepted: ', file)
+    const {
+        add,
+        upload,
+        cancel,
+        isUploaded,
+        isUploading,
+        isError,
+        progress,
+        id,
+        bucketId,
+        name,
+    } = useFileUpload()
 
-        console.log('uploading form')
+    const nhost = React.useContext(NhostContext)
+
+    const onFileAccepted = (file: File) => {
         setFile(file)
+
+        upload({ file }).then((res) => {
+            // console.log('file upload result: ', JSON.parse(JSON.stringify(res)))
+
+            // console.log(`getting public url of id: ${res.id}`)
+
+            const publicUrl = nhost.storage.getPublicUrl({ fileId: res.id! })
+
+            // console.log('publicUrl: ', publicUrl)
+        })
     }
 
     return (
         <Flex className="imageForm" background={file ? 'green' : 'blue'}>
             {file && <p>File: {file.name}</p>}
+            {isUploading && <p>Progress: {progress}...</p>}
             <Button
                 onClick={(e: React.MouseEvent) => {
                     setFile(null)
